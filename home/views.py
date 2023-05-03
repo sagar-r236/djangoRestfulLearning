03ee8ce3246ad02789a6057f9830a40f603c1066
sagar-r236 <sagar236@protonmail.com>
2023-05-03 15:21:53 +0530

    basic over next onto serializer

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def index(request):
+    courses = {
+        'course_name' : 'Python',
+        'learn' : ['Flask', 'Django', 'Fast Api'],
+        'course_provider' : 'Someone'
+    }
+
+    return Response(courses)
