3e61421174edad49f2aa709a4d29fd7b7db54eaf
sagar-r236 <sagar236@protonmail.com>
2023-05-04 12:17:19 +0530

    validation done for foreign key serialization model is created

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import Color, Person
+
+admin.site.register(Color)
+admin.site.register(Person)
--- home/serializers.py
+++ home/serializers.py
@@ -7,3 +7,15 @@ class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Person
         fields = "__all__"
+
+    def validate(self, data):
+
+        special_character = '!@#$%^&*()_-+=<>,./?\'":;{}][|\\'
+        if any(c in special_character for c in data['name']):
+            raise serializers.ValidationError('name cannot contain special character')
+
+        if data['age'] < 18:
+            raise serializers.ValidationError('age should be greater than 18')
+        return data
+    
+
